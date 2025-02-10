81c60b45d3d165ad79701d17143ec9d830cab89c
AndySVK <andil.andrej@gmail.com>
2025-02-10 12:24:40 -0500

    finished secodn tastk third in progress

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -16,23 +16,40 @@ public_users.get('/',function (req, res) {
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  const ISBN = req.params.isbn;
-  for (let key in books) {
-    if (books[key].isbn === ISBN) {
-        return books[key];
-    } else {
-        //finish no book found under the ISBN
+public_users.get('/isbn/:isbn', function (req, res) {
+    const ISBN = req.params.isbn;
+    let bookInfo = null;
+
+    for (let key in books) {
+        if (books[key].isbn === ISBN) {
+            bookInfo = books[key];
+            break;
+        }
     }
-}
-  return res.status(300).json({message: "Yet to be implemented"});
- });
+
+    if (bookInfo) {
+        return res.send("The book details for ISBN number: " + ISBN + ", are: " + JSON.stringify(bookInfo));
+    } else {
+        return res.status(404).send("No books found for the ISBN: " + ISBN);
+    }
+});
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/author/:author', function (req, res) {
+    const author = req.params.author.toLowerCase(); // Convert to lowercase for case-insensitive comparison
+    let booksByAuthor = [];
+
+    for (let key in books) {
+        if (books[key].author.toLowerCase() === author) {
+            booksByAuthor.push(books[key]);
+        }
+    }
+
+    if (booksByAuthor.length > 0) {
+        return res.json(booksByAuthor);
+    } else {
+        return res.status(404).send("No books found for the author: " + req.params.author);
+    }
 });
 
 // Get all books based on title
