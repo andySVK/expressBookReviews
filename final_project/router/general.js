9cc5a3d89efa044826865aeba2bda292539893bc
AndySVK <andil.andrej@gmail.com>
2025-02-11 16:56:09 -0500

    Task 7 in progress

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -4,10 +4,36 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
+const doesExist = (username) => {
+    // Filter the users array for any user with the same username
+    let userswithsamename = users.filter((user) => {
+        return user.username === username;
+    });
+    // Return true if any user with the same username is found, otherwise false
+    if (userswithsamename.length > 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const username = req.body.username;
+    const password = req.body.password;
+
+    // Check if both username and password are provided
+    if (username && password) {
+        // Check if the user does not already exist
+        if (!doesExist(username)) {
+            // Add the new user to the users array
+            users.push({"username": username, "password": password});
+            return res.status(200).json({message: "User successfully registered. Now you can login"});
+        } else {
+            return res.status(404).json({message: "User already exists!"});
+        }
+    }
+    // Return error if username or password is missing
+    return res.status(404).json({message: "Unable to register user."});
 });
 
 // Get the book list available in the shop
@@ -18,17 +44,17 @@ public_users.get('/',function (req, res) {
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn', function (req, res) {
     const ISBN = req.params.isbn;
-    let bookInfo = null;
+    let bookByIsbn = null;
 
     for (let key in books) {
         if (books[key].isbn === ISBN) {
-            bookInfo = books[key];
+            bookByIsbn = books[key];
             break;
         }
     }
 
-    if (bookInfo) {
-        return res.send("The book details for ISBN number: " + ISBN + ", are: " + JSON.stringify(bookInfo));
+    if (bookByIsbn) {
+        return res.send("The book details for ISBN number: " + ISBN + ", are: " + JSON.stringify(bookByIsbn));
     } else {
         return res.status(404).send("No books found for the ISBN: " + ISBN);
     }
@@ -46,7 +72,7 @@ public_users.get('/author/:author', function (req, res) {
     }
 
     if (booksByAuthor.length > 0) {
-        return res.json(booksByAuthor);
+        return res.send("This is/are the book(s) by this author: " + JSON.stringify(booksByAuthor));
     } else {
         return res.status(404).send("No books found for the author: " + req.params.author);
     }
@@ -54,14 +80,37 @@ public_users.get('/author/:author', function (req, res) {
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const title = req.params.title.toLowerCase();
+  let bookByTitle;
+
+  for (let key in books){
+    if(books[key].title.toLowerCase() === title){
+         bookByTitle = books[key];
+         break;
+    }
+  }
+  if(bookByTitle){
+    return res.send("This is the book information for title: " +req.params.title + ", " + bookByTitle)
+  }else{
+    return res.status(404).json({message: "No book found under this title"});
+  }
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbnForRev = req.params.isbn;
+  let reviews;
+  for (let key in books){
+    if(books[key].isbn === isbnForRev){
+        review = books[key].reviews;
+        break;
+    }
+  }
+  if(review){
+    return res.send("These are the reviews for isbn: " + req.params.isbn + ", " + reviews)
+  }else{
+    return res.status(404).json({message: "No book or review found under this isbn"});
+  }
 });
 
 module.exports.general = public_users;
